a5887e38edaba98e9be081fbff22f10efa2b081d
Paul Mooney <pmooney@architech.ca>
2022-09-29 14:31:13 -0400

    Initial Commit. Working with Remote1 app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
+  {path: 'testremote1', loadChildren: () =>
+    loadRemoteModule({
+      type: 'manifest',
+      remoteName: 'remote1', // Key in mf.manifest.json
+      exposedModule: './RemoteStuffModule' // key in remote project's webpack.config.js `exposes` object
+    }).then(m => m.RemotestuffModule) // Same module name as in remote1 project
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
